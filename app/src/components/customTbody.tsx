dc90a5db015ba3a1e528718a78b82cd7a9f61e3a
PabloEnguix09 <pabloenguixllopis@gmail.com>
2024-10-27 19:12:04 +0100

    Upgraded style and correct format of texts

--- app/src/components/customTbody.tsx
+++ app/src/components/customTbody.tsx
@@ -8,15 +8,17 @@ export default function CustomTbody<T extends object>(props: Props<T>) {
 	return (
 		<tbody>
 			{props.data.map((element: object) => (
-				<tr
-					key={
-						element !== undefined
-							? `operation_${Object.values(element)[0]}`
-							: ''
-					}
-				>
+				<tr className="text-center" key={element !== undefined ? `operation_${Object.values(element)[0]}` : ''}>
 					{Object.entries(element).map((value) => (
-						<td key={value[0]}>{value[1]}</td>
+						<td key={value[0]}>
+							{value[0] === 'fecha'
+								? new Date(value[1]).toLocaleDateString('es-ES', {
+										year: 'numeric',
+										month: 'long',
+										day: 'numeric',
+								  })
+								: value[1]}
+						</td>
 					))}
 				</tr>
 			))}
--- app/src/components/customThead.tsx
+++ app/src/components/customThead.tsx
@@ -7,9 +7,9 @@ interface Props {
 export default function CustomThead(props: Props) {
 	return (
 		<thead className="table-dark">
-			<tr>
+			<tr className="text-center">
 				{props.keys.map((key) => (
-					<th key={key}>{key.toLocaleUpperCase()}</th>
+					<th key={key}>{key.toLocaleUpperCase().split('_').join(' ')}</th>
 				))}
 			</tr>
 		</thead>
